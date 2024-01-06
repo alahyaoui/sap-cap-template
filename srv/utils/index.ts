64cc1d455bad4d2944d01890795b85891badd6d4
Ayoub Lahyaoui <ayoub.lahyaoui@capgemini.com>
2024-01-06 00:19:10 +0000

    feat: Add utils index file

---
+++ srv/utils/index.ts
@@ -0,0 +1,4 @@
+export { default as ArrayUtils } from "./array-utils";
+export { default as DateUtils } from "./date-utils";
+export { default as EntityUtils } from "./entity-utils";
+export { default as ObjectUtils } from "./object-utils";
